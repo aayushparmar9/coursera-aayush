36c8014d792667d5d7b7bca2b1d795cc5195d8c9
Aayush <aayushparmar9@gmail.com>
2017-03-22 12:25:35 +0530

    Rename script.js to question.js

---
+++ js/question.js
@@ -0,0 +1,30 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function StartTest(){
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = 
+                res.firstName + " " + 
+              if (res.Question1==="true") {
+                message += " You have passed the test";
+              }
+              else {
+                message += " You have failed the test";
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays + 1;
+              message += " displays for coding.";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
+  }
+);
